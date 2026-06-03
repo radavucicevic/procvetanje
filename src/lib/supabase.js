428e2259494444742a7e55781df3d827b9fe7729
radavucicevic <info@radavucicevic.net>
2026-06-03 19:38:40 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://zijbzghopnkhyyknwwrq.supabase.co';
+const supabaseKey = 'OVDE_NALEPI_TVOJ_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
